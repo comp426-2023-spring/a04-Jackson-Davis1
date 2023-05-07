58e310acbc06a40ad6ff2a2fb10ae87c13a0cf87
Jackson <jdavis19@ad.unc.edu>
2023-05-07 01:37:59 -0400

    final

--- server.js
+++ server.js
@@ -12,24 +12,29 @@ const port = args.port || 5000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
-app.get('/app', (req, res) => {
+app.get('/app', (res) => {
     res.status(200).send('200 OK');
 })
 
-app.get('/app/rps', (req, res) => {
+app.get('/app/rps', (res) => {
     res.status(200).send(rps());
 })
 
-app.get('/app/rpsls', (req, res) => {
+app.get('/app/rpsls', (res) => {
     res.status(200).send(rpsls());
+})
+
+app.get('/app/rpsls/play', (req, res) => {
+    res.status(200).send(rpsls(req.query.shot));
 })
 
 app.get('/app/rps/play', (req, res) => {
     res.status(200).send(rps(req.query.shot));
 })
 
-app.get('/app/rpsls/play', (req, res) => {
-    res.status(200).send(rpsls(req.query.shot));
+
+app.get('/app/rps/play/:shot', (req, res) => {
+    res.status(200).send(rps(req.params.shot));
 })
 
 app.post('/app/rps/play', (req, res) => {
@@ -40,18 +45,13 @@ app.post('/app/rpsls/play', (req, res) => {
     res.status(200).send(rpsls(req.body.shot));
 })
 
-
-app.get('/app/rps/play/:shot', (req, res) => {
-    res.status(200).send(rps(req.params.shot));
-})
+app.get('*', (res) => {
+    res.status(404).send('404 NOT FOUND');
+  });
 
 app.get('/app/rpsls/play/:shot', (req, res) => {
     res.status(200).send(rpsls(req.params.shot));
 })
-
-app.get('*', (req, res) => {
-    res.status(404).send('404 NOT FOUND');
-  });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
