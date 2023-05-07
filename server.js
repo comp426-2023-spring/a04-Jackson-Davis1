d4e943cc0f365ab1c133c30b88a2ef1453640896
Jackson <jdavis19@ad.unc.edu>
2023-05-07 01:47:42 -0400

    final

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 #!/usr/bin/env node
+
 import minimist from "minimist"
 import express from "express" 
 import { rps, rpsls } from "./lib/rpsls.js"
 
 
-let app = express();
-let args = minimist(process.argv.slice(2));
-let port = args.port || 5000;
+const app = express();
+const args = minimist(process.argv.slice(2));
+const port = args.port || 5000;
 
 app.use(express.json());
-
 app.use(express.urlencoded({extended: true}))
 
-app.get('/app', (res) => {
+app.get('/app', (req, res) => {
     res.status(200).send('200 OK');
 })
 
-app.get('/app/rps', (res) => {
+app.get('/app/rps', (req, res) => {
     res.status(200).send(rps());
 })
 
-app.get('/app/rpsls', (res) => {
+app.get('/app/rpsls', (req, res) => {
     res.status(200).send(rpsls());
 })
 
@@ -39,6 +39,7 @@ app.post('/app/rps/play', (req, res) => {
 app.post('/app/rpsls/play', (req, res) => {
     res.status(200).send(rpsls(req.body.shot));
 })
+
 
 app.get('/app/rps/play/:shot', (req, res) => {
     res.status(200).send(rps(req.params.shot));
