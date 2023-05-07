c9ebf2811ccd3837b02127c8273e3fb911c3e6b9
Jackson <jdavis19@ad.unc.edu>
2023-05-07 01:45:20 -0400

    final

--- server.js
+++ server.js
@@ -1,51 +1,51 @@
 #!/usr/bin/env node
-
 import minimist from "minimist"
 import express from "express" 
 import { rps, rpsls } from "./lib/rpsls.js"
 
 
-const app = express();
-const args = minimist(process.argv.slice(2));
-const port = args.port || 5000;
+let app = express();
+let args = minimist(process.argv.slice(2));
+let port = args.port || 5000;
 
 app.use(express.json());
+
 app.use(express.urlencoded({extended: true}))
 
 app.get('/app', (res) => {
     res.status(200).send('200 OK');
 })
 
+app.get('/app/rps', (res) => {
+    res.status(200).send(rps());
+})
+
 app.get('/app/rpsls', (res) => {
     res.status(200).send(rpsls());
-})
-
-app.get('/app/rpsls/play', (req, res) => {
-    res.status(200).send(rpsls(req.query.shot));
-})
-
-app.post('/app/rpsls/play', (req, res) => {
-    res.status(200).send(rpsls(req.body.shot));
-})
-
-app.get('/app/rpsls/play/:shot', (req, res) => {
-    res.status(200).send(rpsls(req.params.shot));
 })
 
 app.get('/app/rps/play', (req, res) => {
     res.status(200).send(rps(req.query.shot));
 })
 
-app.get('/app/rps', (res) => {
-    res.status(200).send(rps());
+app.get('/app/rpsls/play', (req, res) => {
+    res.status(200).send(rpsls(req.query.shot));
 })
 
 app.post('/app/rps/play', (req, res) => {
     res.status(200).send(rps(req.body.shot));
 })
 
+app.post('/app/rpsls/play', (req, res) => {
+    res.status(200).send(rpsls(req.body.shot));
+})
+
 app.get('/app/rps/play/:shot', (req, res) => {
     res.status(200).send(rps(req.params.shot));
+})
+
+app.get('/app/rpsls/play/:shot', (req, res) => {
+    res.status(200).send(rpsls(req.params.shot));
 })
 
 app.get('*', (req, res) => {
@@ -53,5 +53,5 @@ app.get('*', (req, res) => {
   });
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-  })
+  console.log(`Listening on port ${port}`)
+})
